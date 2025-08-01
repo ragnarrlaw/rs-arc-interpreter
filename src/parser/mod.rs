114de46d52ffac2614ba1c0826d6e7f609c3b797
ragnarrlaw <carmichealclair@yahoo.com>
2025-08-01 19:27:58 +0530

    refactor: update Parser to use Box<dyn Diagnostic> for error handling and enhance parse_program logic

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,7 +1,10 @@
 use crate::{
-    error::IntprError,
-    lexer::{Lexer, token::Token},
+    error::{diagnostic::Diagnostic},
+    lexer::{
+        span::Span, token::{ Token, TokenType}, Lexer
+    },
     line_map::LineMap,
+    parser::ast::{Expression, Program, Statement},
 };
 
 pub mod ast;
@@ -20,7 +23,7 @@ impl<'a> Parser<'a> {
         source: &'a str,
         lexer: &'a mut Lexer<'a>,
         line_map: &'a LineMap,
-    ) -> Result<Self, IntprError<'a>> {
+    ) -> Result<Self, Box<dyn Diagnostic>> {
         let mut parser = Parser {
             source,
             lexer,
@@ -33,38 +36,113 @@ impl<'a> Parser<'a> {
         Ok(parser)
     }
 
-    fn advance(&mut self) -> Result<(), IntprError<'a>> {
+    fn advance(&mut self) -> Result<(), Box<dyn Diagnostic>> {
         let t = self.lexer.next_token()?;
         self.curr_token = self.peek_token.take();
         self.peek_token = Some(t);
         Ok(())
     }
 
-    pub fn parse_program(&mut self) -> Result<ast::Program<'a>, IntprError<'a>> {
+    pub fn parse_program(&mut self) -> Result<ast::Program<'a>, Box<dyn Diagnostic>> {
+        let mut stmts: Vec<Statement<'a>> = vec![];
+        while self
+            .curr_token
+            .as_ref()
+            .is_some_and(|t| t.t_type != TokenType::Eof)
+        {
+            stmts.push(self.parse_statement()?);
+            self.advance()?;
+        }
+        Ok(Program { statements: stmts })
+    }
+
+    fn parse_statement(&mut self) -> Result<ast::Statement<'a>, Box<dyn Diagnostic>> {
+        let token = self.curr_token.as_ref().unwrap();
+        match token.t_type {
+            TokenType::Let => self.parse_let_statement(),
+            TokenType::Return => self.parse_return_statement(),
+            TokenType::Function => self.parse_fn_definition(),
+            _ => self.parse_expression_statement(),
+        }
+    }
+
+    /*
+     * let := 10;
+     * let x 10;
+     * */
+    fn parse_let_statement(&mut self) -> Result<ast::Statement<'a>, Box<dyn Diagnostic>> {
         todo!()
     }
 
-    fn parse_statement(&mut self) -> Result<ast::Statement<'a>, IntprError<'a>> {
+    fn parse_return_statement(&mut self) -> Result<ast::Statement<'a>, Box<dyn Diagnostic>> {
         todo!()
     }
 
-    fn parse_let_statement(&mut self) -> Result<ast::Statement<'a>, IntprError<'a>> {
+    /*
+     * expressions that appear at the hightest level
+     *
+     * >> 5
+     * >> 10 + 10
+     * >> 10
+     * >> "string"
+     * >> {
+     * >>   let x = 10;
+     * >>   x + 20
+     * >> }
+     *
+     * */
+    fn parse_expression_statement(&mut self) -> Result<ast::Statement<'a>, Box<dyn Diagnostic>> {
         todo!()
     }
 
-    fn parse_return_statement(&mut self) -> Result<ast::Statement<'a>, IntprError<'a>> {
+    /*
+     * fn function_definition(param_1, param_2, param_3) {
+     *    // code
+     * }
+     * */
+    fn parse_fn_definition(&mut self) -> Result<ast::Statement<'a>, Box<dyn Diagnostic>> {
         todo!()
     }
 
-    fn parse_expression_statement(&mut self) -> Result<ast::Statement<'a>, IntprError<'a>> {
+    fn parse_expression(&mut self) -> Result<ast::Expression<'a>, Box<dyn Diagnostic>> {
         todo!()
     }
 
-    fn parse_fn_definition(&mut self) -> Result<ast::Statement<'a>, IntprError<'a>> {
+    fn curr_token_is(&self, t_type: TokenType) -> bool {
+        self.curr_token.as_ref().is_some_and(|t| t.t_type == t_type)
+    }
+
+    fn peek_token_is(&self, t_type: TokenType) -> bool {
+        self.peek_token.as_ref().is_some_and(|t| t.t_type == t_type)
+    }
+
+    fn jump_to_sync_point(&mut self, sync_tokens: Vec<TokenType>) {
+        while self
+            .curr_token
+            .as_ref()
+            .is_some_and(|t| t.t_type != TokenType::Eof)
+        {
+            if self
+                .curr_token
+                .as_ref()
+                .is_some_and(|t| sync_tokens.contains(&t.t_type))
+            {
+                break;
+            }
+        }
+    }
+
+    // fn function_def (param1, param2, param3)
+    //                  ^                    ^
+    // read paramter alists
+    fn read_function_params(&mut self) -> Result<Vec<String>, Box<dyn Diagnostic>> {
         todo!()
     }
 
-    fn parse_expression(&mut self) -> Result<ast::Expression<'a>, IntprError<'a>> {
+    // call(arg1, arg2, arg3)
+    //      ^              ^
+    // read function argument list
+    fn read_function_args(&mut self) -> Result<Vec<Expression<'a>>, Box<dyn Diagnostic>> {
         todo!()
     }
 }
@@ -84,7 +162,7 @@ mod tests {
         let foobar := 8383;";
 
         let line_map = LineMap::new(source);
-        let mut lexer = Lexer::new(source, &line_map);
+        let mut lexer = Lexer::new(source);
 
         let parser_res = Parser::new(source, &mut lexer, &line_map);
         match parser_res {
@@ -130,7 +208,34 @@ mod tests {
                 };
             }
             Err(err) => {
-                panic!("{err}")
+                panic!("{}", err.message())
+            }
+        };
+    }
+
+    #[test]
+    fn test_parse_return_statements() {
+        let source = "return 10;
+        return x; 
+        return add(10, 20);";
+
+        let line_map = LineMap::new(source);
+        let mut lexer = Lexer::new(source);
+
+        let parser_res = Parser::new(source, &mut lexer, &line_map);
+        match parser_res {
+            Ok(mut parser) => {
+                match parser.parse_program() {
+                    Ok(program) => {
+                        assert_eq!(program.statements.len(), 3);
+                    }
+                    Err(_err) => {
+                        panic!("parser failed to identify the let statements")
+                    }
+                };
+            }
+            Err(err) => {
+                panic!("{}", err.message())
             }
         };
     }
