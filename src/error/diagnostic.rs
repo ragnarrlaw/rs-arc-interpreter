efd43917be13315f3755c53b625d9e300d727062
ragnarrlaw <carmichealclair@yahoo.com>
2025-08-08 19:40:52 +0530

    refactor: implement Display trait for Diagnostic to enhance error message formatting

--- src/error/diagnostic.rs
+++ src/error/diagnostic.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use std::fmt::Display;
 
 use crate::lexer::span::Span;
 
@@ -10,5 +11,11 @@ pub trait Diagnostic: fmt::Debug {
     // optional helper message
     fn help(&self) -> Option<String> {
         None
+    }
+}
+
+impl Display for dyn Diagnostic {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message())
     }
 }
