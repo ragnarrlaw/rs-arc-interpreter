f27f73d2d799a1df17dc54206cd659bc90e0f02c
ragnarrlaw <carmichealclair@yahoo.com>
2025-08-24 18:09:59 +0530

    add default function to populate spans with defatul values -> mainly for the built-in types

--- src/lexer/span.rs
+++ src/lexer/span.rs
@@ -1,7 +1,20 @@
+use std::usize;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Span {
     pub start_byte_pos: usize,
     pub end_byte_pos: usize,
     pub line_num: usize,
     pub col_num: usize,
+}
+
+impl Span {
+    pub fn default() -> Self {
+        Self {
+            start_byte_pos: 0,
+            end_byte_pos: 0,
+            line_num: 1,
+            col_num: 1,
+        }
+    }
 }
